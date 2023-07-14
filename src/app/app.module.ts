8eb793025162098fe95caaaa0770af6902dbee24
nishamokashe <nishamokase18@gmail.com>
2023-07-14 07:35:49 +0530

    app.module.ts
    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
