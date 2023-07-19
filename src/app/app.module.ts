f7c60a37ad9e3544abc41bbebb526718a5a341c2
nishamokashe <nishamokase18@gmail.com>
2023-07-19 09:41:50 +0530

    app.module.ts
    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
+import { AssigmentComponent } from './assigment/assigment.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    DatabindingComponent
+    DatabindingComponent,
+    ChildComponent,
+    AssigmentComponent,
+    NgIfComponent,
+    NgForComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
