219d1f64f079d3434e1735fddf0a8fde5ef4fded
nishamokashe <nishamokase18@gmail.com>
2023-07-19 07:44:51 +0530

    app.component.ts
    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularApp';
+   title = 'angularApp7';
+  
+    data: string = 'Angular Batch 7';
+
+   getDataFromChild(value){
+   console.log(value);
+   
+   }
 }
