9375841447448c38d693bcf213e91ecd6501e8c6
nishamokashe <nishamokase18@gmail.com>
2023-07-19 09:43:44 +0530

    databinding.component.ts
    databinding.component.ts

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -9,19 +9,20 @@ export class DatabindingComponent implements OnInit {
 
  name:string ='Jack Sparrow';
 
+ imageURL:string = 'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8='
+ isDisabled = false;
  data ={
   id:'101',
   name:'Harry'
  }
 
- imageURL:string = 'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8='
-  isDisabled = false;
+ deparment: string = 'IT'
  constructor() { }
 
   ngOnInit() {
   }
 
-  SaveData(){
-    alert('Button Clicked');
-  }
+  // SaveData(){
+  //   alert('Button Clicked');
+  // }
 }
