50de765497e6ca3d83929e716a55fd0a8edd22e3
nishamokashe <nishamokase18@gmail.com>
2023-08-03 01:06:23 +0530

    app.module.ts
    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { DatabindingComponent } from './databinding/databinding.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChildComponent } from './child/child.component';
 import { AssigmentComponent } from './assigment/assigment.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
@@ -16,6 +16,10 @@ import { AttributeDirectiveComponent } from './attribute-directive/attribute-dir
 import { CustomdirDirective } from './customdir.directive';
 import { SimpleformComponent } from './simpleform/simpleform.component';
 import { FormAssignmentComponent } from './form-assignment/form-assignment.component';
+import { SimpleformoneComponent } from './simpleformone/simpleformone.component';
+import { ReactiveformComponent } from './reactiveform/reactiveform.component';
+import { TasktempletFromComponent } from './tasktemplet-from/tasktemplet-from.component';
+
 
 @NgModule({
   declarations: [
@@ -31,12 +35,17 @@ import { FormAssignmentComponent } from './form-assignment/form-assignment.compo
     AttributeDirectiveComponent,
     CustomdirDirective,
     SimpleformComponent,
-    FormAssignmentComponent
+    FormAssignmentComponent,
+    SimpleformoneComponent,
+    ReactiveformComponent,
+    TasktempletFromComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
