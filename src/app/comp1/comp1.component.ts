edcabd14dc19cae583cfae3e2b7a047cff05031f
nishamokashe <nishamokase18@gmail.com>
2023-08-27 07:52:49 +0530

    comp1.component.ts
    comp1.component.ts

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { ServicelService } from '../Services/servicel.service';
+import { Service1Service } from '../Services/service1.service';
+import { UserService } from '../Services/user.service';
+
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.css']
 })
-export class Comp1Component implements OnInit {
 
+export class Comp1Component implements OnInit {
   products = {};
-  // I have injected my serevice into constructor
-  constructor(private objService:ServicelService) { 
+  newArray:any =[];
+  constructor(private objService :Service1Service, private objUser: UserService) { 
+    //const objService = new Service1Service();
+    this.products = objService.products;
   }
 
- ngOnInit() {
-  // const objService = new ServicelService();
-   this.products = this.objService.products;
- }
+  ngOnInit() {
+    var obj = this.objUser.getUsers();
+    obj.subscribe(res=> {
+    console.log(res);
+    this.newArray=res;
+    console.log(this.newArray[0].address.street)
+   })
+  }
 }
